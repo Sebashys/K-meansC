c799edd948f92b41aee3b456faaf308c250d1dbb
Sebashys <shoyoslatam@gmail.com>
2019-07-03 12:37:04 -0500

    iris result and timmer

--- K-means_Rn.cpp
+++ K-means_Rn.cpp
@@ -1,50 +1,63 @@
-#include <algorithm>
-#include <stdlib.h>
-#include <limits>
-#include <random>
-#include <vector>
+#include "timer.hh"
+#include <tuple>
+#include <string>
+#include <fstream>
+#include <sstream>
 #include <iostream>
+#include <vector>
+#include <random>
 
-const RN = 2
+using namespace std;
 
 
-struct Point { // puntos en una dimencion
-	std::vector<size_t>	// vector de enteros
-};
+using point = vector<double>;
 
-using DataFrame = std::vector<Point>; // vector de puntos (vector de vectores)
+using DataFrame = vector<Point>; // vector de puntos (vector de vectores)
 
-double square(double value){ // cuadrado de un valor
+inline double square(double value){ // cuadrado de un valor
 	return value * value;
 }
 
-double squared_12_distance(Point first, Point second){// vectores del mismo tamaño (troubleshooting)
+inline double squared_12_distance(Point first, Point second){// vectores del mismo tamaño (troubleshooting)
 	for (int i = 0; i < first.size(); i++){ // recore todo el vector para sacar el cuadrado de las distancias de 2 puntos
 		aux += square(first[i] - second[i]);			
 	}
 	return aux;
 }
 
-// va hasta aqui el trabajo hehco
-DataFrame k_means( const DataFrame& data, size_t k, size_t number_of_iterations){
-					   
-	static std::random_device seed;
-	static std::mt19937 random_number_generator(seed());
-	std::uniform_int_distribution<size_t> indices(0,data.size()-1);		  
-	
-	
+
+pair<DataFrame,vector<size_t>> k_means( const DataFrame& data, size_t k, size_t number_of_iterations, double ep){
+	size_t dimensions = data[0].size();
+		   
+	size_t dimensions = data[0].size();
+	// en proximo codigo colocar data.size/nvariables		   
+	static random_device seed;
+	static mt19937 random_number_generator(seed());
+	uniform_int_distribution<size_t> indices(0,data.size()-1);/// change		  
 	// pick centroids as random points from the dataset
-	DataFrame means(k);
-	for (auto& cluster : means){ // cluster -> means
-		cluster = data[indices(random_number_generator)];
-	}				   
+	DataFrame means(k);// K*nvariables
+	
+	double distanciaepsilon;
+	size_t contador;
+	size_t epsilon = numeric_limits<size_t>::max();
+
+	
+	for (Point& cluster : means){ // cluster -> means
+		size_t i = indices(random_number_generator);
+		cluster = data[i];//data rango i nvariable tener en cuenta ultimo rango y primero		
+	}
 
 
-	std::vector<size_t>assignments(data.size());
-	for(size_t iteration = 0; iteration < number_of_iterations; ++iteration){
+	vector<size_t> assignments(data.size());
+
+	for(size_t iteration = 0; iteration < number_of_iterations; iteration++){
+
+		if(ep > epsilon ){
+			return {means, assignments};
+		}
 		// find assignements
-		for (size_t point = 0; point < data.size() ; ++point){
-			double best_distance = std::numeric_limits<double>::max();// variable mejor distacia, inicializada con la maxima
+		for (size_t point = 0; point < data.size() ; point++){
+			double best_distance = numeric_limits<double>::max();// variable mejor distacia, inicializada con la maxima
 			size_t best_cluster = 0; // variable mejor cluster, inicializada con 0
 			for (size_t cluster = 0; cluster < k; ++cluster){
 				const double distance = squared_12_distance(data[point], means[cluster]);
@@ -56,43 +69,95 @@ DataFrame k_means( const DataFrame& data, size_t k, size_t number_of_iterations)
 		assignments[point] = best_cluster;
 		}
 		
-		DataFrame new_means(k);
-		std::vector<size_t> counts(k, 0);
-		for (size_t point = 0; point < data.size(); ++point){
-		    const auto cluster = assignments[point];
-		    new_means[cluster].x += data[point].x;
-		    new_means[cluster].y += data[point].y;			
+		DataFrame new_means(k,vector<double>(dimensions,0.0));
+		DataFrame new_meansaux(k,vector<double>(dimensions,0.0));
+		vector<size_t> counts(k, 0);
+
+		for (size_t point = 0; point < data.size(); point++){
+		    const size_t cluster = assignments[point];
+		    for(size_t d = 0; d < dimensions; d++){
+		    	new_means[cluster][d] += data[point][d];
+		    }			
 			counts[cluster] += 1;
 		}
-		
+
 		// divide sumas por saltos para obtener centroides
-		for (size_t cluster = 0; cluster < k; ++cluster){
-			const auto count = std::max<size_t>(1, counts[cluster]);
-			means[cluster].x = new_means[cluster].y / count;
-			means[cluster].y = new_means[cluster].y / count;
-			
+		for (size_t cluster = 0; cluster < k; cluster++){
+			const size_t count = max<size_t>(1, counts[cluster]);
+			for(size_t d = 0; d < dimensions;d++){
+				new_meansaux[cluster][d] = means[cluster][d];
+				means[cluster][d] = new_means[cluster][d] / count;
+			}
+			distanciaepsilon = squared_12_distance(new_meansaux[cluster],means[cluster]);
+			//cout << new_meansaux[cluster][0] <<'|'<< new_meansaux[cluster][1] <<'|'<< new_meansaux[cluster][2]<< endl;
+			//cout << means[cluster][0] <<'|'<< means[cluster][1] <<'|'<< means[cluster][2]<< endl;
+			if(distanciaepsilon < ep){
+				//cout << "exito" << endl;
+				contador++;
+			}
+			else{//cout << "menor 1" << endl;
+				contador = 0;}
+			if(contador > k){
+				cout << iteration <<endl;
+				return {means, assignments};
+			}			
 		}
-	}	
-	return means;
+	}
+	return {means, assignments};
+}
+
+DataFrame readData(string File,int nVariables ){
+	DataFrame data;
+	ifstream input(File);
+	string line;
+	while(getline(input,line)){
+		istringstream iss(line);
+		double v;
+		Point p;
+		for(int i = 0;i < nVariables; i++){
+			iss >> v;
+			p.push_back(v);
+			}
+		data.push_back(p);
+		}
+		cout << data.size() << endl;
+		return data;
+}
+
+void imprimirkameans(vector<size_t> m,DataFrame data,int k){
+	vector<int> v(k);
+	cout << "prueba" << endl; 
+	for(int i = 0; i < data.size(); i++) {
+  	//cout << "point " << i << " -> " << a[i] << endl;
+	//cout << m[i] <<endl;
+	//cout << data[i][0]<<'|'<<data[i][1]<< '|'<< data[i][2]<<'|'<<data[i][3]<< " -> "<< m[i] <<endl;
+  	v[m[i]]++;
+  	}
+
+  	cout << v.size() << endl;
+  	for(int x = 0; x<v.size(); x++){
+  		cout << "k_means" << x << " -> "<<v[x] <<endl;
+  	}
+
+
 }
 
 int main(){
-	// main para generar un data set random (mejorar con un dataset externo)
-	DataFrame SampleData(50);
-	for(size_t i= 0; i < 50; i++){
-		SampleData[i].x = rand() % (100);
-		SampleData[i].y = rand() % (100);
-		//muestra en pantalla el dataset
-		std::cout << SampleData[i].x << " "  << SampleData[i].y << "\n" ;
-	}
+	// main
+	cout << "k_means"<< endl;
+	DataFrame data = readData("iris.data",4);
+	cout << data.size() << endl;
+	DataFrame c;
+	vector<size_t> a;
+	//cout << data << endl;
+	ScopedTimer t;
+
+	tie(c,a) = k_means(data,3,100000,0.0);
+	cout <<  " tiempo : " << t.elapsed()<< "ms" << endl;
+
+	imprimirkameans(a,data,3);
+
 	
-	DataFrame kmeans1(3);
-	kmeans1 = k_means(SampleData, 3 , 4); // k means con 3 centroides y 4 iteraciones
-	std::cout << "K-MEANS" << "\n";
-	//muestra resutado
-	for(size_t i= 0; i < 3; i++){
-		
-		//muestra en pantalla el kmeans
-		std::cout << kmeans1[i].x << " "  << kmeans1[i].y << "\n" ;
-	}
+	return 0;
+
 }
